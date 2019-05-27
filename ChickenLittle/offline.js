212e21c3120bd2e9d9bb260382369568173febca
fariashel <44454981+fariashel@users.noreply.github.com>
2019-05-26 21:24:57 -0300

    Add files via upload

---
+++ ChickenLittle/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1558916433,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/stars-sheet0.png",
+		"images/spaceshipcontroller-sheet0.png",
+		"images/bulletcontroller-sheet0.png",
+		"images/enemycontroller-sheet0.png",
+		"images/bulletcontoller1-sheet0.png",
+		"images/arrowupbutton-sheet0.png",
+		"images/arrowupbutton-sheet1.png",
+		"images/shootbutton-sheet0.png",
+		"images/shootbutton-sheet1.png",
+		"images/arrowdownbutton-sheet0.png",
+		"images/arrowdownbutton-sheet1.png",
+		"images/arrowrigthbutton-sheet0.png",
+		"images/arrowrigthbutton-sheet1.png",
+		"images/arrowleftbutton-sheet0.png",
+		"images/arrowleftbutton-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
